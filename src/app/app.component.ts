19673c1c9a942cd2d65781f1971a8ec3ac2986d1
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-09-19 16:05:01 -0300

    Componentes de seções implementados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SobreComponent } from "./components/secoes/sobre/sobre.component";
+import { HabilidadesComponent } from "./components/secoes/habilidades/habilidades.component";
+import { StackComponent } from "./components/secoes/stack/stack.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [NgClass],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    SobreComponent,
+    HabilidadesComponent,
+    StackComponent,
+  ],
   templateUrl: "app.component.html",
-  styleUrl: "app.component.scss"
 })
-export class AppComponent {
-  public colapsarNavbar: boolean = false
-}
+export class AppComponent {}
